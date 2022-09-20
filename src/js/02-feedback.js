21efb53f1fac90e91cdea47a8b80a757302ee1bf
Anna Lisitsyna <anna.lisitsyna93@gmail.com>
2022-09-20 20:45:38 +0200

    add tasks

--- src/js/02-feedback.js
+++ src/js/02-feedback.js
@@ -7,6 +7,7 @@ const STORAGE_KEY = 'feedback-msg';
 const refs = {
   form: document.querySelector('.js-feedback-form'),
   textarea: document.querySelector('.js-feedback-form  textarea'),
+  input: document.querySelector('.js-feedback-form input'),
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
@@ -53,13 +54,28 @@ function populateTextarea() {
 // Домой
 // сделать так чтобы сохраняло не только сообщение но и имя, и все в одном обьекте
 
+// populateTextarea();
+
 // const formData = {};
 
 // refs.form.addEventListener('input', e => {
-//   // console.log(e.target.name);
-//   // console.log(e.target.value);
-
 //   formData[e.target.name] = e.target.value;
+//   localStorage.setItem('formData', JSON.stringify(formData));
 
-//   console.log(formData);
+//   console.log('formData', formData);
 // });
+
+// refs.form.addEventListener('submit', e => {
+//   e.preventDefault();
+//   e.currentTarget.reset();
+//   localStorage.removeItem('formData');
+// });
+
+// function populateTextarea() {
+//   const savedData = JSON.parse(localStorage.getItem('formData'));
+
+//   if (savedData) {
+//     refs.textarea.value = savedData.message;
+//     refs.input.value = savedData.name;
+//   }
+// }
